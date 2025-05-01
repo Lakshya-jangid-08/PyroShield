afa5f9f3cc479e4335e433e390d0b48bc3a61848
lakshya jangid IIITV <202351075@iiitvadodara.ac.in>
2025-05-01 19:21:22 +0530

    updateDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 require("dotenv").config();
+const fs = require("fs");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,6 +8,9 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.PORT,
+  ssl: {
+    rejectUnauthorized: true  // or false, if you don't have certs yet
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
